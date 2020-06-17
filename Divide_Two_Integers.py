cb1c122709c6d308a2b1b3c725e79b886cd98e4c
Felix Zhao <felixzhao@users.noreply.github.com>
2020-06-17 22:51:34 +1200

    Update Divide_Two_Integers.py

--- Divide_Two_Integers.py
+++ Divide_Two_Integers.py
@@ -1,5 +1,7 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
         turn = False
         if dividend < 0:
             dividend = -dividend
@@ -8,10 +10,20 @@ class Solution:
             divisor = -divisor
             turn = not turn
         i = 0
-        while dividend - divisor >= 0:
-            dividend -= divisor
-            diviosr = divisor << 1
-            i += 1
+        while dividend >= divisor:
+            """
+            t = 9
+            j = 4
+            div = 1
+            i = 2
+            
+            """
+            temp, j = divisor, 1
+            while dividend >= temp:
+                dividend -= temp
+                temp <<= 1
+                i += j # add j step
+                j <<= 1
         if turn:
             i = -i
         return i
