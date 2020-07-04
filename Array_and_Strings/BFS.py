cd6a44427e5b7a525f5f51dae8b5ad43adfdf9bf
Felix Zhao <felixz2010@gmail.com>
2020-07-05 11:38:34 +1200

    update indentation.

--- Array_and_Strings/BFS.py
+++ Array_and_Strings/BFS.py
@@ -1,29 +1,29 @@
 # Function to print a BFS of graph 
-    def BFS(self, s): 
-        # time complexity : O(V + E)
-        # Mark all the vertices as not visited 
-        visited = [False] * (len(self.graph)) 
-  
-        # Create a queue for BFS 
-        queue = [] 
-  
-        # Mark the source node as  
-        # visited and enqueue it 
-        queue.append(s) 
-        visited[s] = True
-  
-        while queue: 
-  
-            # Dequeue a vertex from  
-            # queue and print it 
-            s = queue.pop(0) 
-            print (s, end = " ") 
-  
-            # Get all adjacent vertices of the 
-            # dequeued vertex s. If a adjacent 
-            # has not been visited, then mark it 
-            # visited and enqueue it 
-            for i in self.graph[s]: 
-                if visited[i] == False: 
-                    queue.append(i) 
-                    visited[i] = True
+def BFS(self, s):
+    # time complexity : O(V + E)
+    # Mark all the vertices as not visited
+    visited = [False] * (len(self.graph))
+
+    # Create a queue for BFS
+    queue = []
+
+    # Mark the source node as
+    # visited and enqueue it
+    queue.append(s)
+    visited[s] = True
+
+    while queue:
+
+        # Dequeue a vertex from
+        # queue and print it
+        s = queue.pop(0)
+        print (s, end = " ")
+
+        # Get all adjacent vertices of the
+        # dequeued vertex s. If a adjacent
+        # has not been visited, then mark it
+        # visited and enqueue it
+        for i in self.graph[s]:
+            if visited[i] == False:
+                queue.append(i)
+                visited[i] = True
