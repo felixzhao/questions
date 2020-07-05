255a34cf60c5319ab2d18ec284dd03498f0341f4
Felix Zhao <felixz2010@gmail.com>
2020-07-05 14:13:47 +1200

    clean up code

--- Array_and_Strings/Roman_to_Integer.py
+++ Array_and_Strings/Roman_to_Integer.py
@@ -14,8 +14,8 @@ class Solution:
         if first < second sub
         add up last node before return
         """
-        dic = {'I':1, 'V'             :5,'X'             :10,'L'             :50,'C'             :100,'D'             :500,'M' :            1000              }
-        
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
         value = 0
         for i in range(len(s) - 1):
             if dic[s[i]] >= dic[s[i+1]]:
