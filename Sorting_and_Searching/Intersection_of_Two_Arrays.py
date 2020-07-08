b3f6b3a7b63ae714d37d8d84b2064dbfb708cec2
Felix Zhao <felixz2010@gmail.com>
2020-07-08 21:52:49 +1200

    intersection two arrays, with two set method.

--- Sorting_and_Searching/Intersection_of_Two_Arrays.py
+++ Sorting_and_Searching/Intersection_of_Two_Arrays.py
@@ -18,3 +18,43 @@ class Solution:
 
         """
         return set(nums1).intersection(set(nums2))
+
+    def set_intersection(self, set1, set2) -> List[int]:
+        return [x for x in set1 if x in set2]
+
+    def intersectionTwoSet(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        """
+        Two Set
+
+        logic
+            - Do Not use official implement intersection function
+            - check element in set1 and set2
+
+        key points
+            - list comprehension with condition [x for x in A if C]
+
+        Strange thing
+            - use shorter length set at first take less time cost in list comprehension
+        """
+        set1 = set(nums1)
+        set2 = set(nums2)
+        if len(set1) < len(set2):
+            return self.set_intersection(set1, set2)
+        else:
+            return self.set_intersection(set2, set1)
+    # test of call set_intersection vs short first
+    #         start_time = time.time()
+    #         res = self.set_intersection(set1, set2)
+    #         print("--- %s seconds ---" % (time.time() - start_time))
+
+    #         start_time = time.time()
+    #         if len(set1) < len(set2):
+    #             res = self.set_intersection(set1, set2)
+    #         else:
+    #             res = self.set_intersection(set2, set1)
+    #         print("--- %s seconds ---" % (time.time() - start_time))
+    #         return res
+    # --- 2.384185791015625e-06 seconds ---
+    # --- 1.430511474609375e-06 seconds ---
+    # --- 9.5367431640625e-07 seconds ---
+    # --- 1.1920928955078125e-06 seconds ---
