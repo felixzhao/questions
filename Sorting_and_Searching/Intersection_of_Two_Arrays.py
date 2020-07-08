9fea71d47dc95b9ab2a5b821baef68c36107d223
Felix Zhao <felixz2010@gmail.com>
2020-07-08 21:56:05 +1200

    update time complexity.

--- Sorting_and_Searching/Intersection_of_Two_Arrays.py
+++ Sorting_and_Searching/Intersection_of_Two_Arrays.py
@@ -1,7 +1,7 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         """
-        My Solution
+        My Solution (smiliar with answer)
 
         Set Intersection
 
