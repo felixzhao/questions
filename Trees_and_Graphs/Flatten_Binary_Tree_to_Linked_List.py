7e66af1119c91b7fc2fc93ce606e6f509a38e62e
Felix Zhao <felixz2010@gmail.com>
2020-07-19 12:14:26 +1200

    Flatten_Binary_Tree_to_Linked_List add my solution

---
+++ Trees_and_Graphs/Flatten_Binary_Tree_to_Linked_List.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        """
+        My Solution
+
+        Recursive
+
+        logic
+            - recursive left and right branch
+            - if reach empty leaf return
+            - put processed left branch to root.right
+            - find the end of the processed left branch, put processed right branch follow it.
+
+        Key Points
+            - original left branch Must set to None
+
+        """
+        if not root:
+            return None
+        l = self.flatten(root.left)
+        r = self.flatten(root.right)
+        root.left = None
+        p = root
+        if l:
+            root.right = l
+            p = l
+            while p.right:
+                p = p.right
+        if r:
+            p.right = r
+        return root
+
