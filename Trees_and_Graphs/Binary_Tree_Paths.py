156a4e159b6323395d08f2b7e2522b9afbcc28b6
Felix Zhao <felixz2010@gmail.com>
2020-07-22 22:07:43 +1200

    add Binary Tree path Iteration approach.

--- Trees_and_Graphs/Binary_Tree_Paths.py
+++ Trees_and_Graphs/Binary_Tree_Paths.py
@@ -37,3 +37,31 @@ class Solution:
             return []
         self.find(root, f"{root.val}")
         return self.res
+
+    def binaryTreePathsIterations(self, root: TreeNode) -> List[str]:
+        """
+        Iterations
+        (queue FILO or stack FIFO)
+
+        logic
+        - init root as node and root.val as path in queue
+        - add to res when reach leaf
+        - add left and right to queue, if existed
+
+        time  O(N)
+        space O(N)
+        """
+        if not root:
+            return []
+        res = []
+        queue = [(root, f'{root.val}')]
+        while queue:
+            node, path = queue.pop(0)
+            if not node.left and not node.right:
+                res.append(path)
+            if node.left:
+                queue.append((node.left, f'{path}->{node.left.val}'))
+            if node.right:
+                queue.append((node.right, f'{path}->{node.right.val}'))
+        return res
+
